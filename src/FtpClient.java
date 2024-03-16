86eb305830df840862ca597688e78d319023e667
VolticuS6 <100294967+Stuchellbj21@users.noreply.github.com>
2024-03-15 20:15:53 -0400

    Barebones starting code, so we have a proper connection (port number was changed to reflect document)

--- src/FtpClient.java
+++ src/FtpClient.java
@@ -16,5 +16,36 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 public class FtpClient {
-   
+    public static final int PORT = 9001;
+
+    public static final String HOST = "127.0.0.1";
+    //public static final String HOST = "10.37.106.205";
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to GCC Chatting Room");
+        System.out.println("Please enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String user_name = scanner.nextLine().trim();
+
+        try {
+            Socket socket = new Socket(HOST, PORT);
+            System.out.println("Succeed: socket: " + socket);
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                System.out.println(user_name + ": ");
+                String message = scanner.nextLine().trim();
+                message = user_name + ": " + message;
+                outputStream.writeUTF(message);
+                String response = inputStream.readUTF();
+                System.out.println(response);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/FtpServer.java
+++ src/FtpServer.java
@@ -9,7 +9,38 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class FtpServer {
-  
+    public static final int PORT = 9001;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String user_name = scanner.nextLine().trim();
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is listening to " + PORT);
+            Socket socket = serverSocket.accept();
+            System.out.println("Connected: socket " + socket);
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+            do {
+                String message_in = inputStream.readUTF();
+                System.out.println(message_in);
+
+                System.out.println(user_name + ": ");
+                String message_out = scanner.nextLine().trim();
+                message_out = user_name + ": " + message_out;
+                outputStream.writeUTF(message_out);
+
+            }while(true);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
